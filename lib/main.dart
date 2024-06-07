a910a14abc4a1baefdcdedd119de293918925402
hamdyhamdy <hamdykhalid12345@gmail.com>
2024-06-08 00:59:35 +0200

    prevent screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:cmp_app/core/helpers/cache_helper.dart';
 import 'package:cmp_app/my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 import 'firebase_options.dart';
 
@@ -21,4 +24,10 @@ void main() async {
   ]);
 
   runApp(const MyApp());
+
+  WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+    if (Platform.isAndroid) {
+      await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+    }
+  });
 }
